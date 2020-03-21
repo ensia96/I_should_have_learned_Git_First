642ec9147ad1c65a66ccb4b6db68c4300f205b68
최호준 <ensia96@choehojunui-MacBook-Pro.local>
2020-03-21 22:20:10 +0900

    변경쓰 1.3

--- index.php
+++ index.php
@@ -1,44 +1,42 @@
-<!DOCTYPE html>
-<html lang="ko">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>문서띠</title>
-</head>
-<body>
-    <h1><a href="index.php">문서띠</a></h1>
-    <ol>
-    <?php
-    $list = scandir('data');
-    $i = 0;
+<?php
+function title_list() {
+    $list = scandir('./data'); $i = 0;
     while ($i<count($list)) {
         if ($list[$i] != '.') {
-            if($list[$i] != '..') {
-                ?>
+            if($list[$i] != '..') { ?>
                 <li><a href="index.php?id=<?=$list[$i]?>"><?=$list[$i]?></a></li>
-                <?php
-            }
+            <?php }
         }
         $i = $i + 1;
     }
-    ?>  
-    </ol>
-    <h2>
-        <?php
-        if (isset($_GET['id'])) {
-            echo $_GET['id'];
-        } else {
-            echo "어서오세요";
-        }
-        ?>
-    </h2>
-    <?php
+}
+function title() {
+    if (isset($_GET['id'])) {
+        echo $_GET['id'];
+    } else {
+        echo "어서오세요";
+    }
+}
+function contents() {
     if (isset($_GET['id'])) {
         echo nl2br(file_get_contents('data/'.$_GET['id']));
         echo "<p>노오력 : ".strlen(file_get_contents('data/'.$_GET['id']))."점</p>";
     } else {
         echo "Git 공부 늦게해서 지금부터 공부내용 정리한거 올리는 사람입니다.";
     }
-    ?>
-</body>
+}
+?>
+<!DOCTYPE html>
+<html lang="ko">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?=title();?></title>
+    </head>
+    <body>
+        <h1><a href="index.php">문서띠</a></h1>
+        <ol><?=title_list();?>  </ol>
+        <h2><?=title();?></h2>
+        <?=contents();?>
+    </body>
 </html>
