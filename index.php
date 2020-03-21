2a2733bae18a68e2e131d2a9a482ce51dbec24d4
최호준 <ensia96@choehojunui-MacBook-Pro.local>
2020-03-21 21:57:02 +0900

    변경쓰 1.1

--- index.php
+++ index.php
@@ -6,12 +6,24 @@
     <title>문서띠</title>
 </head>
 <body>
-    <h1>문서띠</h1>
+    <h1><a href="index.php">문서띠</a></h1>
     <ol>
         <li><a href="index.php?id="></a></li>
     </ol>
-    <h2><?=$_GET['id']?></h2>
-    <?=nl2br(file_get_contents('data/'.$_GET['id']))?>
-    <p>노오력 : <?=strlen(file_get_contents('data/'.$_GET['id']))?> 점</p>
+    <h2><?php
+    if (isset($_GET['id'])) {
+        echo $_GET['id'];
+    } else {
+        echo "어서오세요";
+    }
+    ?></h2>
+    <?php
+    if (isset($_GET['id'])) {
+        echo nl2br(file_get_contents('data/'.$_GET['id']));
+        echo "<p>노오력 : ".strlen(file_get_contents('data/'.$_GET['id']))."점</p>";
+    } else {
+        echo "Git 공부 늦게해서 지금부터 공부내용 정리한거 올리는 사람입니다.";
+    }
+    ?>
 </body>
 </html>
