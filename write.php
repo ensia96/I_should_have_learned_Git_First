6e9770fa37270e9a9f2e86a010c179bc6cefa8b7
최호준 <ensia96@choehojunui-MacBook-Pro.local>
2020-03-22 10:21:23 +0900

    변경쓰 2.1.1

--- write.php
+++ write.php
@@ -47,15 +47,13 @@ function contents() {
         </style>
     </head>
     <body>
-        <div id="font">
-            <h1><a href="index.php">글 쓰기</a></h1>
-            <ol><?=title_list();?></ol>
-            <h2>돌아가려면 글 쓰기를 누르세요</h2>
-            <form action="write_.php" method="post">
-                <p><input type="text" name="title" placeholder="여기에 제목을 적어주세요." size="55"></p>
-                <p><textarea class = "wrap" name="contents" placeholder="여기엔 내용을 적어주세요." rows="25"></textarea></p>
-                <p><input type="submit" value="저장 쓰 !"></p>
-            </form>
-        </div>
+        <h1><a href="index.php">글 쓰기</a></h1>
+        <ol><?=title_list();?></ol>
+        <h2>돌아가려면 글 쓰기를 누르세요</h2>
+        <form action="write_.php" method="post">
+            <p><input type="text" name="title" placeholder="여기에 제목을 적어주세요." size="55"></p>
+            <p><textarea class = "wrap" name="contents" placeholder="여기엔 내용을 적어주세요." rows="25"></textarea></p>
+            <p><input type="submit" value="저장 쓰 !"></p>
+        </form>
     </body>
 </html>
