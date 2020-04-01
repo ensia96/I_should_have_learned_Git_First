ead64082546260a12c74191efe1ca3b278a436e2
최호준 <ensia96@choehojunui-MacBook-Pro.local>
2020-04-01 14:21:19 +0900

    변경쓰 2.3.0

--- edit.php
+++ edit.php
@@ -4,7 +4,7 @@
             <form action="_edit.php" method="post">
                 <input type="hidden" name="b_title" value="<?=$_GET['id']?>">
                 <p><input class = "wrap" type="text" name="title" placeholder="여기에 제목을 적어주세요." value="<?php title();?>"></p>
-                <p><textarea class = "wrap" name="contents" placeholder="여기엔 내용을 적어주세요." rows="20"><?php contents();?></textarea></p>
+                <p><textarea class = "wrap" name="contents" placeholder="여기엔 내용을 적어주세요." rows="30"><?php contents();?></textarea></p>
                 <p><input type="submit" value="저장 쓰 !"></p>
             </form>
         </div>
